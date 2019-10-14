b2616806a03e63ebf6d7b3e96b2a900dda49c54c
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-10-14 08:45:49 +0800

    Alter company_email and salary fields on JobOffer

--- jobboardapi/jobs/models.py
+++ jobboardapi/jobs/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class JobOffer(models.Model):
     company_name = models.CharField(max_length=60)
-    company_email = models.CharField(max_length=60)
+    company_email = models.EmailField()
     job_title = models.CharField(max_length=60)
     job_description = models.TextField()
-    salary = models.CharField(max_length=60)
+    salary = models.PositiveIntegerField()
     city = models.CharField(max_length=60)
     state = models.CharField(max_length=60)
     created_at = models.DateTimeField(auto_now_add=True)

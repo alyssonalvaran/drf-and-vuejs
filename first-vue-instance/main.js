340f2a0b28f23123b2cb8d157dce1492b2e03174
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-10-17 10:58:08 +0800

    Add first-vue-instance

---
+++ first-vue-instance/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		message: "Hello World!",
+		value: 5,
+		imgSrc: "https://secure.img1-fg.wfcdn.com/im/62945217/resize-h700-p1-w700%5Ecompr-r85/8470/84707680/Pokemon+Pikachu+Wall+Decal.jpg",
+		link: "https://vuejs.org/",
+	}
+})

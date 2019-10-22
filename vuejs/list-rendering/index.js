05589370e0efb5dd7a5bebd14111969bc0099aeb
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-10-23 03:39:38 +0800

    Add list-rendering

---
+++ vuejs/list-rendering/index.js
@@ -0,0 +1,27 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		users: [{
+			id: 1,
+			name: "alice",
+			profession: "developer",
+		},
+		{
+			id: 2,
+			name: "bob",
+			profession: "developer",
+		},
+		{
+			id: 3,
+			name: "sam",
+			profession: "manager",
+		},
+		{
+			id: 4,
+			name: "kate",
+			profession: "designer",
+		},]
+	},
+	methods: {
+	},
+})

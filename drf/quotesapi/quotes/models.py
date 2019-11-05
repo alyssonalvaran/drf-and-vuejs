2698e73307c2f14d1567e802d28321de63f5a062
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-11-05 18:32:02 +0800

    Update Quote model

--- drf/quotesapi/quotes/models.py
+++ drf/quotesapi/quotes/models.py
@@ -3,11 +3,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Quote(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
     quote_author = models.CharField(max_length=60)
     quote_body = models.TextField()
-    context = models.TextField(blank=True, null=True)
-    source = models.CharField(max_length=140, blank=True, null=True)
+    context = models.CharField(max_length=240, blank=True, null=True)
+    source = models.CharField(max_length=120, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.quote_body
+        return self.quote_author

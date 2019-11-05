1f37ab61baaedd8dd933e1c98ce3b5e52885b088
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-11-05 17:49:50 +0800

    Add quote model and register to admin

--- drf/quotesapi/quotes/models.py
+++ drf/quotesapi/quotes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Quote(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    quote_author = models.CharField(max_length=60)
+    quote_body = models.TextField()
+    context = models.TextField(blank=True, null=True)
+    source = models.CharField(max_length=140, blank=True, null=True)
+
+    def __str__(self):
+        return self.quote_body

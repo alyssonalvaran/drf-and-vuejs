314e4ee7df17714627d18dbc27c8a034c83d9cba
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-11-05 18:26:41 +0800

    Add pagination

--- drf/quotesapi/quotes/api/views.py
+++ drf/quotesapi/quotes/api/views.py
@@ -1,10 +1,7 @@
 from rest_framework import generics
-# from rest_framework import permissions
-# from rest_framework.exceptions import ValidationError
-# from rest_framework.generics import get_object_or_404
 
 from quotes.models import Quote
-# from ebooks.api.pagination import SmallSetPagination
+from quotes.api.pagination import QuotePagination
 from quotes.api.permissions import IsAdminUserOrReadOnly
 from quotes.api.serializers import QuoteSerializer
 
@@ -13,8 +10,7 @@ class QuoteListCreateAPIView(generics.ListCreateAPIView):
     queryset = Quote.objects.all().order_by("id")
     serializer_class = QuoteSerializer
     permission_classes = [IsAdminUserOrReadOnly]
-    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    # pagination_class = SmallSetPagination
+    pagination_class = QuotePagination
 
 class QuoteDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Quote.objects.all()

4a50785f43e3cff3599bd62d58bdd485c7cccef1
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-11-21 00:12:54 +0800

    Add sample token header authorization

--- drf/profilesapi/clients/token-auth-test1.py
+++ drf/profilesapi/clients/token-auth-test1.py
@@ -1,14 +1,19 @@
 import requests
 
 def client():
-	credentials = {
-		"username": "alyssonalvaran",
-		"password": "Password0!",
-	}
+	token_h = "Token de49a691a343254d4a11a2bd3e9cc6ae1f0949a6"
 
-	response = requests.post("http://127.0.0.1:8000/api/rest-auth/login/",
-							data=credentials)
+	# credentials = {
+	# 	"username": "alyssonalvaran",
+	# 	"password": "Password0!",
+	# }
 
+	# response = requests.post("http://127.0.0.1:8000/api/rest-auth/login/",
+	# 						data=credentials)
+
+	headers = {"Authorization": token_h}
+	response = requests.get("http://127.0.0.1:8000/api/profiles/",
+							headers=headers)
 
 	print("Status code:", response.status_code)
 	response_data = response.json()

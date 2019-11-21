c99e778bb0b72797b8d66cfc17e6ebac330ceb16
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-11-21 12:04:21 +0800

    Add ProfileViewSet

--- drf/profilesapi/profiles/api/urls.py
+++ drf/profilesapi/profiles/api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from profiles.api.views import ProfileList
+from profiles.api.views import ProfileViewSet
 
 urlpatterns = [
 	path(
 		"profiles/",
-		ProfileList.as_view(),
+		ProfileViewSet.as_view({"get": "list"}),
 		name="profile-list"
+	),
+	path(
+		"profiles/<int:pk>/",
+		ProfileViewSet.as_view({"get": "retrieve"}),
+		name="profile-detail"
 	),
 ]

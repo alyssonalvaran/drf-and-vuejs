033337f669b06daac747a5d96f3d27e95cad5929
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-11-21 12:27:04 +0800

    Add ProfileStatusViewSet and IsOwnerOrReadOnly permission

--- drf/profilesapi/profiles/api/urls.py
+++ drf/profilesapi/profiles/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from profiles.api.views import ProfileViewSet
+from profiles.api.views import ProfileViewSet, ProfileStatusViewSet
 
 router = DefaultRouter()
 router.register(r"profiles", ProfileViewSet)
+router.register(r"status", ProfileStatusViewSet)
 
 urlpatterns = [
 	path("", include(router.urls))

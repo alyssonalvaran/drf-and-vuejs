3c6e8370f1a6cb73ffd83a3095fa1397339da61d
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-11-21 13:33:50 +0800

    ProfileViewSetTestCase

--- drf/profilesapi/profiles/tests.py
+++ drf/profilesapi/profiles/tests.py
@@ -22,3 +22,50 @@ class RegistrationTestCase(APITestCase):
 
 		response = self.client.post("/api/rest-auth/registration/", data)
 		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class ProfileViewSetTestCase(APITestCase):
+
+	list_url = reverse("profile-list")
+
+	def setUp(self):
+		self.user = User.objects.create_user(username="testcase",
+											password="some_strong_pw")
+		self.token = Token.objects.create(user=self.user)
+		self.api_authentication()
+
+	def api_authentication(self):
+		self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
+
+	def test_profile_list_authenticated(self):
+		response = self.client.get(self.list_url)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+	def test_profile_list_unauthenticated(self):
+		self.client.force_authenticate(user=None)
+		response = self.client.get(self.list_url)
+		self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+	def test_profile_detail_retrieve(self):
+		response = self.client.get(reverse("profile-detail", kwargs={"pk": 1}))
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertEqual(response.data["user"], "testcase")
+
+	def test_profile_detail_update_by_owner(self):
+		response = self.client.put(reverse("profile-detail", kwargs={"pk": 1}),
+									{"city": "some_city", "bio": "some bio"})
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertEqual(json.loads(response.content), {
+							"id": 1,
+							"user": "testcase",
+							"city": "some_city",
+							"bio": "some bio",
+							"avatar": None
+						})
+
+	def test_profile_detail_update_by_random_user(self):
+		random_user = User.objects.create_user(username="random", password="some_strong_pw")
+		self.client.force_authenticate(user=random_user)
+		response = self.client.put(reverse("profile-detail", kwargs={"pk": 1}),
+									{"city": "another_city", "bio": "another bio"})
+		self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
